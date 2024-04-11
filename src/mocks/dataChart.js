3b8649e26ce08c866c84a329d8b59ed5ce7b1703
Libra-lv <166310297+Libra-lv@users.noreply.github.com>
2024-04-11 21:29:51 +0700

    Create dataChart.js

---
+++ src/mocks/dataChart.js
@@ -0,0 +1,485 @@
+/* eslint-disable no-loss-of-precision */
+export const dataChart = {
+  metrics: [
+    {
+      name: "sum_retention",
+      variants: [
+        {
+          PValue: null,
+          confidentialInternal: null,
+          mean: 0.9904761904761905,
+          name: "Ab_num_me_start 1",
+          variantType: "BASE",
+        },
+        {
+          PValue: 0.1577940586939371,
+          confidentialInternal: {
+            changes: 0.009615384615384581,
+            lowerBound: 0.0037561241980701766,
+            upperBound: 0.022986893428839337,
+          },
+          mean: 1,
+          name: "Ab_num_me_start 2",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: 0.1577940586939371,
+          confidentialInternal: {
+            changes: 0.23615384615384581,
+            lowerBound: -0.037561241980701766,
+            upperBound: 0.22986893428839337,
+          },
+          mean: 1,
+          name: "Ab_num_me_start 3",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: 0.1577940586939371,
+          confidentialInternal: {
+            changes: 0.009615384615384581,
+            lowerBound: -130,
+            upperBound: -129,
+          },
+          mean: 1,
+          name: "Ab_num_me_start 4",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: 0.1577940586939371,
+          confidentialInternal: {
+            changes: 0.009615384615384581,
+            lowerBound: -5546,
+            upperBound: 5546,
+          },
+          mean: 1,
+          name: "Ab_num_me_start 6",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: 0.1577940586939371,
+          confidentialInternal: {
+            changes: null,
+            lowerBound: -128,
+            upperBound: 128,
+          },
+          mean: 1,
+          name: "Ab_num_me_start 16",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: 0.1577940586939371,
+          confidentialInternal: {
+            changes: 0.009615384615384581,
+            lowerBound: -160,
+            upperBound: 120,
+          },
+          mean: 1,
+          name: "Ab_num_me_start 7",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: 0.1577940586939371,
+          confidentialInternal: {
+            changes: 0.009615384615384581,
+            lowerBound: 100,
+            upperBound: 120,
+          },
+          mean: 1,
+          name: "Ab_num_me_start 8",
+          variantType: "COMPARED",
+        },
+
+        {
+          PValue: 0.1577940586939371,
+          confidentialInternal: {
+            changes: 0.009615384615384581,
+            lowerBound: 70,
+            upperBound: 82,
+          },
+          mean: 1,
+          name: "Ab_num_me_start 10",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: 0.1577940586939371,
+          confidentialInternal: {
+            changes: null,
+            lowerBound: -9.45,
+            upperBound: -4.54,
+          },
+          mean: 1,
+          name: "Ab_num_me_start 11",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: 0.1577940586939371,
+          confidentialInternal: {
+            changes: null,
+            lowerBound: -0.003363338766564487,
+            upperBound: 0.02060471807690925,
+          },
+          mean: 1,
+          name: "Ab_num_me_start 13",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: 0.1577940586939371,
+          confidentialInternal: {
+            changes: null,
+            lowerBound: -0.003363338766564487,
+            upperBound: 0.02060471807690925,
+          },
+          mean: 1,
+          name: "Ab_num_me_start 14",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: 0.1577940586939371,
+          confidentialInternal: {
+            changes: null,
+            lowerBound: -120,
+            upperBound: 190,
+          },
+          mean: 1,
+          name: "Ab_num_me_start 15",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: 0.1577940586939371,
+          confidentialInternal: null,
+          mean: 1,
+          name: "Ab_num_me_start 18",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: 0.1577940586939371,
+          confidentialInternal: {
+            changes: null,
+            lowerBound: null,
+            upperBound: null,
+          },
+          mean: 1,
+          name: "Ab_num_me_start 20",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: 0.1577940586939371,
+          confidentialInternal: {
+            changes: null,
+            lowerBound: null,
+            upperBound: null,
+          },
+          mean: 1,
+          name: "Ab_num_me_start 21",
+          variantType: "COMPARED",
+        },
+      ],
+    },
+    {
+      name: "aha_rate",
+      variants: [
+        {
+          PValue: null,
+          confidentialInternal: null,
+          mean: 0.4380952380952381,
+          name: "Ab_num_me_start1",
+          variantType: "BASE",
+        },
+        {
+          PValue: 0.35214948343593666,
+          confidentialInternal: {
+            changes: 0.10125858123569795,
+            lowerBound: -0.11204449482874583,
+            upperBound: 0.31456165730014174,
+          },
+          mean: 0.4824561403508772,
+          name: "Ab_num_me_start3",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: 0.23904817295524872,
+          confidentialInternal: {
+            changes: 0.13053732567678425,
+            lowerBound: -0.08676787850009769,
+            upperBound: 0.3478425298536662,
+          },
+          mean: 0.49528301886792453,
+          name: "Ab_num_me_start2",
+          variantType: "COMPARED",
+        },
+      ],
+    },
+    {
+      name: "trial_rate",
+      variants: [
+        {
+          PValue: null,
+          confidentialInternal: null,
+          mean: 0,
+          name: "Ab_num_me_start1",
+          variantType: "BASE",
+        },
+        {
+          PValue: null,
+          confidentialInternal: {
+            changes: null,
+            lowerBound: null,
+            upperBound: null,
+          },
+          mean: 0,
+          name: "Ab_num_me_start2",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: null,
+          confidentialInternal: {
+            changes: null,
+            lowerBound: null,
+            upperBound: null,
+          },
+          mean: 0,
+          name: "Ab_num_me_start3",
+          variantType: "COMPARED",
+        },
+      ],
+    },
+    {
+      name: "habit_rate",
+      variants: [
+        {
+          PValue: null,
+          confidentialInternal: null,
+          mean: 0.3047619047619048,
+          name: "Ab_num_me_start1",
+          variantType: "BASE",
+        },
+        {
+          PValue: 0.8839888991029957,
+          confidentialInternal: {
+            changes: 0.021521226415094203,
+            lowerBound: -0.2675578844956617,
+            upperBound: 0.31060033732585013,
+          },
+          mean: 0.3113207547169811,
+          name: "Ab_num_me_start2",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: 0.08752419443438687,
+          confidentialInternal: {
+            changes: -0.23725328947368426,
+            lowerBound: -0.50940980298245,
+            upperBound: 0.03490322403508138,
+          },
+          mean: 0.2324561403508772,
+          name: "Ab_num_me_start3",
+          variantType: "COMPARED",
+        },
+      ],
+    },
+    {
+      name: "pay_rate",
+      variants: [
+        {
+          PValue: null,
+          confidentialInternal: null,
+          mean: 0,
+          name: "Ab_num_me_start1",
+          variantType: "BASE",
+        },
+        {
+          PValue: null,
+          confidentialInternal: {
+            changes: null,
+            lowerBound: null,
+            upperBound: null,
+          },
+          mean: 0,
+          name: "Ab_num_me_start2",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: null,
+          confidentialInternal: {
+            changes: null,
+            lowerBound: null,
+            upperBound: null,
+          },
+          mean: 0,
+          name: "Ab_num_me_start3",
+          variantType: "COMPARED",
+        },
+      ],
+    },
+    {
+      name: "subs_user",
+      variants: [
+        {
+          PValue: null,
+          confidentialInternal: null,
+          mean: 0,
+          name: "Ab_num_me_start1",
+          variantType: "BASE",
+        },
+        {
+          PValue: null,
+          confidentialInternal: {
+            changes: null,
+            lowerBound: null,
+            upperBound: null,
+          },
+          mean: 0,
+          name: "Ab_num_me_start2",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: null,
+          confidentialInternal: {
+            changes: null,
+            lowerBound: null,
+            upperBound: null,
+          },
+          mean: 0,
+          name: "Ab_num_me_start3",
+          variantType: "COMPARED",
+        },
+      ],
+    },
+    {
+      name: "retention",
+      variants: [
+        {
+          PValue: null,
+          confidentialInternal: null,
+          mean: 0.9904761904761905,
+          name: "Ab_num_me_start1",
+          variantType: "BASE",
+        },
+        {
+          PValue: 0.1396883190901933,
+          confidentialInternal: {
+            changes: 0.009615384615384581,
+            lowerBound: -0.003144541719445477,
+            upperBound: 0.02237531095021464,
+          },
+          mean: 1,
+          name: "Ab_num_me_start3",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: 0.1543564334854344,
+          confidentialInternal: {
+            changes: 0.009615384615384581,
+            lowerBound: -0.003616140043634048,
+            upperBound: 0.022846909274403207,
+          },
+          mean: 1,
+          name: "Ab_num_me_start2",
+          variantType: "COMPARED",
+        },
+      ],
+    },
+    {
+      name: "count_song_start",
+      variants: [
+        {
+          PValue: null,
+          confidentialInternal: null,
+          mean: 4.233333333333333,
+          name: "Ab_num_me_start1",
+          variantType: "BASE",
+        },
+        {
+          PValue: 0.16319442532281694,
+          confidentialInternal: {
+            changes: 0.26021393552221056,
+            lowerBound: -0.10601185441247413,
+            upperBound: 0.6264397254568953,
+          },
+          mean: 5.334905660377358,
+          name: "Ab_num_me_start2",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: 0.39515827793401115,
+          confidentialInternal: {
+            changes: 0.15416493990882724,
+            lowerBound: -0.20188307549057413,
+            upperBound: 0.5102129553082286,
+          },
+          mean: 4.885964912280702,
+          name: "Ab_num_me_start3",
+          variantType: "COMPARED",
+        },
+      ],
+    },
+    {
+      name: "setup_rate",
+      variants: [
+        {
+          PValue: null,
+          confidentialInternal: null,
+          mean: 0.4523809523809524,
+          name: "Ab_num_me_start1",
+          variantType: "BASE",
+        },
+        {
+          PValue: 0.6347648067494397,
+          confidentialInternal: {
+            changes: -0.04986149584487534,
+            lowerBound: -0.2555885233049514,
+            upperBound: 0.1558655316152007,
+          },
+          mean: 0.4298245614035088,
+          name: "Ab_num_me_start3",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: 0.32742665960140827,
+          confidentialInternal: {
+            changes: 0.10526315789473684,
+            lowerBound: -0.10540754424326987,
+            upperBound: 0.3159338600327436,
+          },
+          mean: 0.5,
+          name: "Ab_num_me_start2",
+          variantType: "COMPARED",
+        },
+      ],
+    },
+    {
+      name: "aha_rate_2MEd0",
+      variants: [
+        {
+          PValue: null,
+          confidentialInternal: null,
+          mean: 0.7761904761904762,
+          name: "Ab_num_me_start1",
+          variantType: "BASE",
+        },
+        {
+          PValue: 0.5198813889256748,
+          confidentialInternal: {
+            changes: -0.03374233128834358,
+            lowerBound: -0.13650977606015463,
+            upperBound: 0.06902511348346746,
+          },
+          mean: 0.75,
+          name: "Ab_num_me_start3",
+          variantType: "COMPARED",
+        },
+        {
+          PValue: 0.7739559217864427,
+          confidentialInternal: {
+            changes: 0.014874406760041637,
+            lowerBound: -0.08663279637117294,
+            upperBound: 0.1163816098912562,
+          },
+          mean: 0.7877358490566038,
+          name: "Ab_num_me_start2",
+          variantType: "COMPARED",
+        },
+      ],
+    },
+  ],
+};
